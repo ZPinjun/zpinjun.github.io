965c2a58b8159feff7fef88fcbd008e619207cdb
Pinjun Zheng <pinjun.zheng@kaust.edu.sa>
2024-09-30 17:17:20 +0300

    Move to Jekyll.

---
+++ assets/js/common.js
@@ -0,0 +1,44 @@
+// aHR0cHM6Ly9naXRodWIuY29tL2x1b3N0MjYvYWNhZGVtaWMtaG9tZXBhZ2U=
+$(function () {
+    $('img.lazy').Lazy({
+        scrollDirection: 'vertical',
+        effect: 'fadeIn',
+        effectTime: 300,
+        visibleOnly: true,
+        placeholder: "",
+        onError: function(element) {
+            console.log('[lazyload] Error loading ' + element.data('src'));
+        },
+    })
+
+    $('div.lazy').Lazy({
+        scrollDirection: 'vertical',
+        effect: 'fadeIn',
+        effectTime: 300,
+        visibleOnly: false,
+        placeholder: "",
+        onError: function(element) {
+            console.log('[lazyload] Error loading ' + element.data('src'));
+        },
+        afterLoad: function(element) {
+            // set the style to background-size: cover; 
+            element.css('background-size', 'cover');
+        },
+    })
+
+    $('[data-toggle="tooltip"]').tooltip()
+
+    var $grid = $('.grid').masonry({
+        "percentPosition": true,
+        "itemSelector": ".grid-item",
+        "columnWidth": ".grid-sizer"
+    });
+    // layout Masonry after each image loads
+    $grid.imagesLoaded().progress(function () {
+        $grid.masonry('layout');
+    });
+
+    $(".lazy").on("load", function () {
+        $grid.masonry('layout');
+    });
+})
